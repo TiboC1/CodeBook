443892ab06b6229e1178b4a3f548e74da8e8c655
unknown <de_meulemeester_c@hotmail.com>
2020-01-09 12:11:04 +0100

    username

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -24,6 +24,19 @@
                                 @enderror
                             </div>
                         </div>
+                        <div class="form-group row">
+                            <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('UserName') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
 
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
