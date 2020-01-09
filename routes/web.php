899e3bfa528d470cf5b89a518f60977cb35f36e1
Tibo <tibocolman@gmail.com>
2020-01-09 09:22:52 +0100

    fixed routes

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('/main/welcome');
 });
+
+Auth::routes();
+
+Route::get('/main/home', 'HomeController@index')->name('home');
