3f67b3a7a9df666dbf46ed18d5388ef2e8a20fc3
Tibo <tibocolman@gmail.com>
2020-01-16 14:05:43 +0100

    edited follow button

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -130,15 +130,7 @@
         </div>
     </div>
 
-    <button class="btn btn-info btn-sm action-follow" data-id="{{ $user->id }}">
-        <strong>
-            @if(auth()->user()->isFollowing($user))
-                UnFollow
-            @else
-                Follow
-            @endif
-        </strong>
-    </button>
+    
     <follow-button user-id="{{$user->id}}" follows="{{$follows}}"></follow-button>
     <p>{{$followerCount}}</p>
     <p>{{$followingCount}}</p>
