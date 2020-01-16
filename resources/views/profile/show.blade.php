7c6e28c77aec192e684b43c73fcb3d4282c2e871
Steven <steven.nuyttens@gmail.com>
2020-01-16 10:57:14 +0100

    deleted links to friends/photos

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,29 +1,6 @@
 @extends('layouts.app')
 @section('content')
-<header>
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="{{ route('dashboard', '$user') }}">CodeBook</a>
-            </div>
-            
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    
-                    <li><a href="{{ route('logout') }}">Logout</a></li>
-                </ul>
-            </div><!-- /.navbar-collapse -->
-        </div><!-- /.container-fluid -->
-    </nav>
-</header>
+
 <body>
     <div class="container-fluid">
         <div class="row">
@@ -43,16 +20,7 @@
                         <span class="friendship"><a href="" class="btn btn-info" role="button">Want to be my friend?</a></span>
 
                     </div>
-                    <div class="fb-profile-block-menu">
-                        <div class="block-menu">
-                            <ul>
-                                <li><a href="#">Timeline</a></li>
-                                <li><a href="#">Friends</a></li>
-                                <li><a href="#">Photos</a></li>
-                            </ul>
-                        </div>
-
-                    </div>
+                   
                 </div>
             </div>
         </div>
@@ -82,6 +50,11 @@
             </div>
             <div class="col-md-6">
                 <div class="row">
+                    <div class="card" style="width: 38rem;">
+                        <div class="card-header">
+                            <h1>Timeline</h1>
+                        </div>
+                    </div>
                     @foreach ($user->post as $post)
                    
 
