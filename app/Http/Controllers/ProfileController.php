fc89a24caf88f639d917bbe3a194e2d4d316fecf
Tibo <tibocolman@gmail.com>
2020-01-16 10:55:58 +0100

    fixed banner

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -112,7 +112,7 @@ class ProfileController extends Controller
             
             if(request('banner')){
                 $bannerPath = request('banner')->store('profile', 'public');
-                $banner = Image::make(public_path("storage/{$bannerPath}"))->fit(1000, 1000);
+                $banner = Image::make(public_path("storage/{$bannerPath}"))->fit(1500, 600);
                 $banner->save();
                 $data = array_merge($data, ['banner' => $bannerPath]);
             }
