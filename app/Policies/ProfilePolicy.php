d25cf88894bd7b211e0238ee1ad94fa3e658368a
Tibo <tibocolman@gmail.com>
2020-01-17 14:36:30 +0100

    cleaned up controller

--- app/Policies/ProfilePolicy.php
+++ app/Policies/ProfilePolicy.php
@@ -10,41 +10,7 @@ class ProfilePolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view any profiles.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
-    public function viewAny(User $user)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the profile.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Profile  $profile
-     * @return mixed
-     */
-    public function view(User $user, Profile $profile)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can create profiles.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
-    public function create(User $user)
-    {
-        //
-    }
-
-    /**
+     /**
      * Determine whether the user can update the profile.
      *
      * @param  \App\User  $user
@@ -68,27 +34,4 @@ class ProfilePolicy
         return $user->id == $profile->user_id;
     }
 
-    /**
-     * Determine whether the user can restore the profile.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Profile  $profile
-     * @return mixed
-     */
-    public function restore(User $user, Profile $profile)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the profile.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Profile  $profile
-     * @return mixed
-     */
-    public function forceDelete(User $user, Profile $profile)
-    {
-        //
-    }
 }
