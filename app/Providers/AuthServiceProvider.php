32020b9b03de198f183f0511bdec32c6ae5cfe9e
Tibo <tibocolman@gmail.com>
2020-01-22 16:13:16 +0100

    fixed auth

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,6 +14,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Model' => 'App\Policies\ModelPolicy',
+        Post::class => PostPolicy::class,
+        Profile::class => ProfilePolicy::class,
     ];
 
     /**
